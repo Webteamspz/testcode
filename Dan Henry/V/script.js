d2a8d71000a6a74e2f84824a7766f61427af8ab9
Harshjariwala <harshjariwala34.hj@gmail.com>
2023-04-29 11:45:27 +0530

    DHW-002

---
+++ Dan Henry/V/script.js
@@ -0,0 +1,263 @@
+var productImages = [
+  {
+    className: "wpb",
+    strapeImage1: "https://cdn.shopify.com/s/files/1/1238/1014/products/Dan_Henry_1964_Gran_Turismo_White_Panda_Bicompax_No_Date_Extra_1100x.png?v=1598537459",
+    strapeImage2: "https://cdn.shopify.com/s/files/1/1238/1014/products/Dan_Henry_1964_Gran_Turismo_White_Panda_Bicompax_Date_Extra_1100x.png?v=1598537459",
+    withoutDate1: "https://iili.io/HvjmoIp.md.png",
+    withoutDate2: "https://iili.io/HvjmzQI.md.png",
+    withDate1: "https://iili.io/HvjMKwF.md.png",
+    withDate2: "https://iili.io/HvjMfZg.md.png",
+  },
+  {
+    className: "spb",
+    strapeImage1: "https://cdn.shopify.com/s/files/1/1238/1014/products/Dan_Henry_1964_Gran_Turismo_Panda_Bicompax_No_Date_Extra_1100x.png?v=1598537459",
+    strapeImage2: "https://cdn.shopify.com/s/files/1/1238/1014/products/Dan_Henry_1964_Gran_Turismo_Panda_Bicompax_Date_Extra_1100x.png?v=1598537459",
+    withoutDate1: "https://iili.io/HvjyJ0F.md.png",
+    withoutDate2: "https://iili.io/Hvjy3Ja.md.png",
+    withDate1: "https://iili.io/HvjpOSs.md.png",
+    withDate2: "https://iili.io/HvjpkHG.md.png",
+  },
+  {
+    className: "epb",
+    strapeImage1: "https://cdn.shopify.com/s/files/1/1238/1014/products/Dan_Henry_1964_Gran_Turismo_Evil_Panda_Bicompax_No_Date_Extra_1100x.png?v=1598537459",
+    strapeImage2: "https://cdn.shopify.com/s/files/1/1238/1014/products/Dan_Henry_1964_Gran_Turismo_Evil_Panda_Bicompax_Date_Extra_1100x.png?v=1598537459",
+    withoutDate1: "https://iili.io/Hvw9JQ2.md.png",
+    withoutDate2: "https://iili.io/Hvw92BS.md.png",
+    withDate1: "https://iili.io/HvjyMdu.md.png",
+    withDate2: "https://iili.io/HvjyV5b.md.png",
+  },
+  {
+    className: "sb",
+    strapeImage1: "https://cdn.shopify.com/s/files/1/1238/1014/products/Dan_Henry_1964_Gran_Turismo_Silver_Bicompax_No_Date_Extra_1100x.png?v=1598537459",
+    strapeImage2: "https://cdn.shopify.com/s/files/1/1238/1014/products/Dan_Henry_1964_Gran_Turismo_Silver_Bicompax_Date_Extra_1100x.png?v=1598537459",
+    withoutDate1: "https://iili.io/Hvw98n2.md.png",
+    withoutDate2: "https://iili.io/Hvw9SGS.md.png",
+    withDate1: "https://iili.io/Hvw9ayJ.md.png",
+    withDate2: "https://iili.io/Hvw9lTv.md.png",
+  },
+  {
+    className: "sgb",
+    strapeImage1: "https://cdn.shopify.com/s/files/1/1238/1014/products/Dan_Henry_1964_Gran_Turismo_Slate_Grey_Bicompax_No_Date_Extra_1100x.png?v=1598537459",
+    strapeImage2: "https://cdn.shopify.com/s/files/1/1238/1014/products/Dan_Henry_1964_Gran_Turismo_Slate_Grey_Bicompax_Date_Extra_1100x.png?v=1598537459",
+    withoutDate1: "https://iili.io/HvwH2wv.md.png",
+    withoutDate2: "https://iili.io/HvwHz9s.md.png",
+    withDate1: "https://iili.io/HvwH2wv.md.png",
+    withDate2: "https://iili.io/HvwH3tR.md.png",
+  },
+  {
+    className: "wpt",
+    strapeImage1: "https://cdn.shopify.com/s/files/1/1238/1014/products/Dan_Henry_1964_Gran_Turismo_White_Panda_No_Date_Extra_1100x.png?v=1598537459",
+    strapeImage2: "https://cdn.shopify.com/s/files/1/1238/1014/products/Dan_Henry_1964_Gran_Turismo_White_Panda_Date_Extra_1100x.png?v=1598537459",
+    withoutDate1: "https://iili.io/Hvw3iQf.md.png",
+    withoutDate2: "https://iili.io/Hvw3LB4.md.png",
+    withDate1: "https://iili.io/Hvw3CXf.md.png",
+    withDate2: "https://iili.io/Hvw3ns4.md.png",
+  },
+  {
+    className: "spt",
+    strapeImage1: "https://cdn.shopify.com/s/files/1/1238/1014/products/Dan_Henry_1964_Gran_Turismo_Panda_No_Date_Extra_1100x.png?v=1598537459",
+    strapeImage2: "https://cdn.shopify.com/s/files/1/1238/1014/products/Dan_Henry_1964_Gran_Turismo_Panda_Date_Extra_1100x.png?v=1598537459",
+    withoutDate1: "https://iili.io/HvwFXF2.md.png",
+    withoutDate2: "https://iili.io/HvwFhaS.md.png",
+    withDate1: "https://iili.io/HvwF53N.md.png",
+    withDate2: "https://iili.io/HvwF7aI.md.png",
+  },
+  {
+    className: "ept",
+    strapeImage1: "https://cdn.shopify.com/s/files/1/1238/1014/products/Dan_Henry_1964_Gran_Turismo_Evil_Panda_No_Date_Extra_1100x.png?v=1598537459",
+    strapeImage2: "https://cdn.shopify.com/s/files/1/1238/1014/products/Dan_Henry_1964_Gran_Turismo_Evil_Panda_Date_Extra_1100x.png?v=1598537459",
+    withoutDate1: "https://iili.io/HvwKlUB.md.png",
+    withoutDate2: "https://iili.io/HvwK1HP.md.png",
+    withDate1: "https://iili.io/HvwKfVf.md.png",
+    withDate2: "https://iili.io/HvwKqP4.md.png",
+  },
+  {
+    className: "st",
+    strapeImage1: "https://cdn.shopify.com/s/files/1/1238/1014/products/Dan_Henry_1964_Gran_Turismo_Silver_No_Date_Extra_1100x.png?v=1598537459",
+    strapeImage2: "https://cdn.shopify.com/s/files/1/1238/1014/products/Dan_Henry_1964_Gran_Turismo_Silver_Date_Extra_1100x.png?v=1598537459",
+    withoutDate1: "https://iili.io/HvwKpWb.md.png",
+    withoutDate2: "https://iili.io/HvwKysj.md.png",
+    withDate1: "https://iili.io/HvwKWxa.md.png",
+    withDate2: "https://iili.io/HvwKXWJ.md.png",
+  },
+  {
+    className: "sgt",
+    strapeImage1: "https://cdn.shopify.com/s/files/1/1238/1014/products/Dan_Henry_1964_Gran_Turismo_Slate_Grey_No_Date_Extra_1100x.png?v=1598537459",
+    strapeImage2: "https://cdn.shopify.com/s/files/1/1238/1014/products/Dan_Henry_1964_Gran_Turismo_Slate_Grey_Date_Extra_1100x.png?v=1598537459",
+    withoutDate1: "https://iili.io/HvwfvrQ.md.png",
+    withoutDate2: "https://iili.io/HvwfUYB.md.png",
+    withDate1: "https://iili.io/HvwfErl.md.png",
+    withDate2: "https://iili.io/HvwfM22.md.png",
+  }
+];
+​
+function waitForElement(selector, callback) {
+  var element = document.querySelector(selector);
+  if (element) {
+    // Element found, call the callback function
+    callback(element);
+  } else {
+    // Element not found, wait for 100 milliseconds and then check again
+    setTimeout(function () {
+      waitForElement(selector, callback);
+    }, 100);
+  }
+}
+​
+// Usage example: wait for an element with id "myElement" to appear on the page
+waitForElement("#ProductPhoto .lightbox-img", function (element) {
+  $('body').addClass('dhw-1964');
+  $('body').append(`<script src="
+        https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js
+        "></script>
+        <link href="
+        https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css
+        " rel="stylesheet">`);
+​
+  $('#ProductPhoto').append(`<div class="productWrapper">    
+          <div class="slider">
+            <div class="one img-wrapper">
+              <div class="strape-change-wrapper"><div class="strape-change">Check 2nd strap</div></div>
+            </div>
+            <div class="two img-wrapper">
+            </div>
+            <div class="img-wrapper">
+              <img src="https://iili.io/HvjMBna.md.png"
+                  alt="1964">
+            </div>
+            <div class="img-wrapper">
+              <img src="https://iili.io/HvjMFu1.png"
+                  alt="1964">
+            </div>
+            <div class="img-wrapper">
+              <img src="https://iili.io/HvjMCMJ.md.png"
+                  alt="1964">
+            </div>
+            <div class="img-wrapper">
+              <img src="https://iili.io/HvkTN6B.md.png"
+                  alt="1964">
+            </div>
+          </div>
+          <div class="thumbnail-slider">
+          <div class="one thumbnail-wrapper">
+            </div>
+            <div class="two thumbnail-wrapper">
+            </div>
+            <div class="thumbnail-wrapper">
+              <img src="https://iili.io/HvjMBna.md.png"
+                  alt="1964">
+            </div>
+            <div class="thumbnail-wrapper">
+              <img src="https://iili.io/HvjMFu1.png"
+                  alt="1964">
+            </div>
+            <div class="thumbnail-wrapper">
+              <img src="https://iili.io/HvjMCMJ.md.png"
+                  alt="1964">
+            </div>
+            <div class="thumbnail-wrapper">
+              <img src="https://iili.io/HvkTN6B.md.png"
+                  alt="1964">
+            </div>
+          </div>
+        </div>`);
+​
+  for (let i = 0; i < 10; i++) {
+    $('#ProductPhoto .slider .one').append('<img class="withDate ' + productImages[i].className + '" src="' + productImages[i].withDate1 + '" alt="1964"><img class="withoutDate ' + productImages[i].className + '" src="' + productImages[i].withoutDate1 + '" alt="1964"><img class="strapeImageWithoutDate ' + productImages[i].className + '" src="' + productImages[i].strapeImage1 + '" alt="1964"><img class="strapeImageWithDate ' + productImages[i].className + '" src="' + productImages[i].strapeImage2 + '" alt="1964">');
+    $('#ProductPhoto .slider .two').append('<img class="withDate ' + productImages[i].className + '" src="' + productImages[i].withDate2 + '" alt="1964"><img class="withoutDate ' + productImages[i].className + '" src="' + productImages[i].withoutDate2 + '" alt="1964">');
+    $('#ProductPhoto .thumbnail-slider .one').append('<img class="withDate ' + productImages[i].className + '" src="' + productImages[i].withDate1 + '" alt="1964"><img class="withoutDate ' + productImages[i].className + '" src="' + productImages[i].withoutDate1 + '" alt="1964"><img class="strapeImageWithoutDate ' + productImages[i].className + '" src="' + productImages[i].strapeImage1 + '" alt="1964"><img class="strapeImageWithDate ' + productImages[i].className + '" src="' + productImages[i].strapeImage2 + '" alt="1964">');
+    $('#ProductPhoto .thumbnail-slider .two').append('<img class="withDate ' + productImages[i].className + '" src="' + productImages[i].withDate2 + '" alt="1964"><img class="withoutDate ' + productImages[i].className + '" src="' + productImages[i].withoutDate2 + '" alt="1964">');
+  }
+	$('#productDetails').attr('productName', 'GranTurismoWhitePandaBicompaxNoDate');
+  $.getScript('https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js', function () {
+    $(document).ready(function () {
+      // Initialize main slider
+      $('.slider').slick({
+        dots: false,
+        arrows: true,
+        prevArrow: '<svg class="previous slider-arrow" width="16" height="28" viewBox="0 0 16 28" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14 26L2 14L14 2" stroke="#BB8D51" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+        nextArrow: '<svg class="next slider-arrow" width="16" height="28" viewBox="0 0 16 28" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 2L14 14L2 26" stroke="#BB8D51" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+        infinite: false,
+        speed: 500,
+        adaptiveHeight: false,
+        fade: true,
+        cssEase: 'linear',
+        asNavFor: '.thumbnail-slider',
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              dots: true,
+              arrows: false
+            }
+          }
+        ]
+      });
+​
+      // Initialize thumbnail slider
+      $('.thumbnail-slider').slick({
+        dots: false,
+        arrows: false,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        focusOnSelect: true,
+        asNavFor: '.slider',
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 4
+            }
+          },
+          {
+            breakpoint: 576,
+            settings: {
+              slidesToShow: 3
+            }
+          }
+        ]
+      });
+​
+      // Image click
+      $('.slider .img-wrapper img').on('click', function () {
+        $('#ProductPhotoImg').click();
+        $('.mfp-img').attr('src', $(this).attr('src'));
+      });
+​
+      // Image click
+      $('.strape-change').on('click', function () {
+        $('.hide-1964').click();
+        $(this).text($('.hide-1964').text());
+      });
+    });
+  });
+​
+  // Select the element you want to observe
+  const targetElement = document.querySelector('#ProductPhoto .lightbox-img');
+​
+  // Create a MutationObserver instance
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      // Handle the mutation as needed
+      var product = $('#ProductPhoto .lightbox-img').attr('href').split('1964_')[1];
+      if(product.indexOf('Extra') > 0) {
+    	var productName = product.split('xtra')[0].replaceAll('_', '');
+      } else {
+      	var productName = product.split('.png')[0].replaceAll('_', '');
+      	console.log('?????',productName);
+      }
+      $('#productDetails').attr('productName', productName);
+      if($('.productWrapper .slider.slick-initialized').length > 0) {
+      $('.productWrapper .slider').slick("slickGoTo", 0);
+      }
+    });
+  });
+​
+  // Configuration for the observer 
+  const config = {
+    attributes: true, // Observe changes to attributes
+  };
+​
+  // Start observing the target element
+  observer.observe(targetElement, config);
+});
