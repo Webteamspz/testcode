457e6b27fb5b8596a5d1aea09b33c51561d9b128
Harshjariwala <harshjariwala34.hj@gmail.com>
2023-05-03 10:12:42 +0530

    SS-007 Test

---
+++ Sonno/SS-007 Cart/V/v2-script.js
@@ -0,0 +1,25 @@
+convert.$(document).ready(function () {
+    if (!convert.$('body').hasClass('sonno-test')) {
+        convert.$('body').addClass('sonno-test');
+        loadTest();
+    }
+    function loadTest() {
+    	var width = window.innerWidth;
+    	if (width > 768) {
+			convert.$('<div class="custom-wrap"></div>').insertAfter('.cart_content_info.middle-section .product_btm_detail_div .assembly_buttons');
+    	} else{
+    		convert.$('<div class="custom-wrap"></div>').insertAfter('.cart_content_info.middle-section');
+    	}
+		convert.$('.custom-wrap').append(convert.$('.five.columns.alpha .product-quantity-box'));
+		convert.$('.custom-wrap').append(convert.$('.product_btm_detail_div > .Productcartprice'));
+		convert.$('button.variant_modal').insertBefore('.custom-wrap .product-quantity-box .quantity-wrapper-c');
+    	var price = convert.$('.youSavedPrice').text().split('You saved ')[1];
+    	convert.$('<div class="price-wrapper">You saved <span class="save-price">'+price+'</span></div>').insertAfter('.youSavedPrice');
+    	convert.$('#mattfont .diffcolor').html('<br class="desk-only"> Save Upto £359.99');
+    	if (width < 481) {
+    		convert.$('.insinstall_ment_div:eq(0)').insertBefore(".custom_checkout.mobilebutton");
+    	}else{
+    		convert.$('.insinstall_ment_div:eq(0)').insertAfter(".cart_savings");
+    	}
+    }
+});
