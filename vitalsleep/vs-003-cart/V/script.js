ac402cd49f08aaadd4cee62242acc565acc9468c
raahul09 <raahul09786@gmail.com>
2023-06-21 11:47:56 +0530

    vital updates

--- vitalsleep/vs-003-cart/V/script.js
+++ vitalsleep/vs-003-cart/V/script.js
@@ -57,79 +57,66 @@
             }, 100);
         }
     };
+
+    function changefunc() {
+        if (convert.$('#slidecarthq .slidecarthq .header-review').length == 0) {
+            convert.$('#slidecarthq .slidecarthq').prepend('<div class="header-review"><img src="https://cdn-3.convertexperiments.com/uf/10007840/10007257/1685598184Group+9.png"><p>1,000+ Positive Reviews</p></div>');
+        }
+        // }, 600);
+        if (convert.$("#slidecarthq .slidecarthq .review-item-img").length == 0) {
+            convert.$('<div class="review-item-img"><img src="https://cdn-3.convertexperiments.com/uf/10007840/10007257/1685600303service.png"></div>').insertBefore('#slidecarthq .slidecarthq  .footer');
+        }
+    }
+
     function init() {
         convert.$(document).ready(function () {
             convert.$('body').addClass('vs-03');
-            convert.$('button.add-me-to-cart.add-to-cart-button.orange').on('click', function (e) {
-                waitForElm('#slidecarthq .slidecarthq').then(function (e) {
-                    // setTimeout(() => {
-                    if (convert.$('#slidecarthq .slidecarthq .header-review').length == 0) {
-                            convert.$('#slidecarthq .slidecarthq').prepend('<div class="header-review"><img src="https://cdn-3.convertexperiments.com/uf/10007840/10007257/1685598184Group+9.png"><p>1,000+ Positive Reviews</p></div>');
-                        }
-                        // }, 600);
-                         if (convert.$("#slidecarthq .slidecarthq .review-item-img").length == 0) {
-                        convert.$('<div class="review-item-img"><img src="https://cdn-3.convertexperiments.com/uf/10007840/10007257/1685600303service.png"></div>').insertBefore('#slidecarthq .slidecarthq  .footer');
-                    
-                    }
-                })
-
-            })
             if (location.pathname == '/cart') {
-                convert.$('#shopify-section-header .section-header').append(`
-                <div class="free-ship-box">
-                <div class="client-section" bis_skin_checked="1">
-    <div class="container" bis_skin_checked="1">
-        <div class="client-row" bis_skin_checked="1">
-            <div class="client-col" bis_skin_checked="1">
-                <div class="client-img" bis_skin_checked="1"><img
-                        src="https://cdn-3.convertexperiments.com/uf/10007840/10007257/1685950559one.png"
-                        alt="100% Money Back Guarantee"></div>
-                <div class="client-text" bis_skin_checked="1">
-                    <h4 class="client-title">
-                        <p>100% Money Back Guarantee</p>
-                    </h4>
-                </div>
-            </div>
-            <div class="client-col" bis_skin_checked="1">
-                <div class="client-img Shipping_" bis_skin_checked="1"><img
-                        src="https://cdn-3.convertexperiments.com/uf/10007840/10007257/1685950559two.png"
-                        alt="Free Same Day Shipping"></div>
-                <div class="client-text" bis_skin_checked="1">
-                    <h4 class="client-title">
-                        <p>Free Same Day Shipping</p>
-                    </h4>
-                </div>
-            </div>
-            <div class="client-col" bis_skin_checked="1">
-                <div class="client-img" bis_skin_checked="1"><img
-                        src="https://cdn-3.convertexperiments.com/uf/10007840/10007257/1685950560three.png"
-                        alt="Safe and Secure Checkout"></div>
-                <div class="client-text" bis_skin_checked="1">
-                    <h4 class="client-title">
-                        <p>Safe and Secure Checkout</p>
-                    </h4>
-                </div>
-            </div>
-        </div>
-    </div>
-</div></div>`);
-                convert.$('.checkout_item_block .checkout_buy_row').prepend(`
-                <div class="review-card">
-  <img class="card-img-top" src="https://cdn-3.convertexperiments.com/uf/10007840/10007257/1685953796yellowstar.png" alt="">
-  <p>1,000+ Positive Reviews</p>
- 
-  <div class="card-body">
-    <img class="card-img-top" src="https://cdn-3.convertexperiments.com/uf/10007840/10007257/1685953796yellowstar.png" alt="Rating">
-    <h4 class="card-title">VitalSleep is vital</h4>
-    <p class="card-text">My husband uses this wonderful product! His snoring was not good for his own health and it was keeping me awake at night. From the very first night of using Vitalsleep he has had not snored!</p>
-    <span>- Denise D.</span>
-  </div>
-</div>`)
+                convert.$('#shopify-section-header .section-header').append(`<div class="free-ship-box"><div class="client-section" bis_skin_checked="1"><div class="container" bis_skin_checked="1"><div class="client-row" bis_skin_checked="1"><div class="client-col" bis_skin_checked="1"><div class="client-img" bis_skin_checked="1"><img src="https://cdn-3.convertexperiments.com/uf/10007840/10007257/1685950559one.png" alt="100% Money Back Guarantee"></div><div class="client-text" bis_skin_checked="1"><h4 class="client-title"><p>100% Money Back Guarantee</p></h4></div></div><div class="client-col" bis_skin_checked="1"><div class="client-img Shipping_" bis_skin_checked="1"><img src="https://cdn-3.convertexperiments.com/uf/10007840/10007257/1685950559two.png" alt="Free Same Day Shipping"></div><div class="client-text" bis_skin_checked="1"><h4 class="client-title"><p>Free Same Day Shipping</p></h4></div></div><div class="client-col" bis_skin_checked="1"><div class="client-img" bis_skin_checked="1"><img src="https://cdn-3.convertexperiments.com/uf/10007840/10007257/1685950560three.png" alt="Safe and Secure Checkout"></div><div class="client-text" bis_skin_checked="1"><h4 class="client-title"><p>Safe and Secure Checkout</p></h4></div></div></div></div></div></div>`);
+
+                convert.$('.checkout_item_block .checkout_buy_row').prepend(`<div class="review-card"><img class="card-img-top" src="https://cdn-3.convertexperiments.com/uf/10007840/10007257/1685953796yellowstar.png" alt=""><p>1,000+ Positive Reviews</p><div class="card-body"><img class="card-img-top" src="https://cdn-3.convertexperiments.com/uf/10007840/10007257/1685953796yellowstar.png" alt="Rating"><h4 class="card-title">VitalSleep is vital</h4><p class="card-text">My husband uses this wonderful product! His snoring was not good for his own health and it was keeping me awake at night. From the very first night of using Vitalsleep he has had not snored!</p><span>- Denise D.</span></div></div>`);
+            }
+            if (convert.$('button.add-me-to-cart.add-to-cart-button.orange').length >= 1) {
+                convert.$('button.add-me-to-cart.add-to-cart-button.orange').on('click', function (e) {
+                    waitForElm('#slidecarthq .slidecarthq').then(function (e) {
+                        setTimeout(() => {
+                            if (document.querySelectorAll('.review-item-img').length == 0 && document.querySelectorAll('.slidecarthq-overlay.open').length == 1) {
+                                changefunc();
+                                running = true;
+                                setTimeout(function () { running = false; }, 50000);
+
+                            }
+                        }, 500)
+                    })
+                })
+                const targetNode = document.querySelector("body");
+                const observerConfig = { childList: true, characterData: true, subtree: true };
+
+                const mutationHandler = function (mutationRecords) {
+                    mutationRecords.forEach(function (mutation) {
+                        console.log(mutation.type);
+                    });
+                };
+
+                const myObserver = new MutationObserver(mutationHandler);
+                myObserver.observe(targetNode, observerConfig);
+
+                let running = false;
+                const target = document.body;
+                const config = { childList: true, characterData: true, subtree: true, attributes: true };
+
+                const callback = function (mutationsList, observer) {
+                    if (running) return;
+                    if (document.querySelectorAll('.cartEmpty .review-item-img').length == 1 && document.querySelectorAll('.cartEmpty .empty').length == 1) {
+                        document.querySelector('.cartEmpty .review-item-img').remove();
+                    }
+                    if (document.querySelectorAll('.review-item-img').length == 0 && document.querySelectorAll('.slidecarthq-overlay.open').length == 1) { changefunc(); }
+                };
+                const observer = new MutationObserver(callback);
+                observer.observe(target, config);
             }
         });
-    }
-
-
+    };
     waitForJQuery(function () {
         $(document).ready(function () {
             console.info('convert loaded');
