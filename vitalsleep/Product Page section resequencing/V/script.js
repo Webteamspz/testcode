d468e0d17ad4a0057f0015bf9afdf0fb6db0f1bf
Harshjariwala <harshjariwala34.hj@gmail.com>
2023-07-05 15:12:00 +0530

    VS test added

---
+++ vitalsleep/Product Page section resequencing/V/script.js
@@ -0,0 +1,24 @@
+convert.$(document).ready(() => {
+	const classAllocation = (selector,classPrefix) => {
+	    let sectionIndex = 1;
+	    convert.$(selector).each(function(){
+	        convert.$(this).addClass(classPrefix+'-'+sectionIndex);
+	        sectionIndex++;
+	    });
+	};
+	const orderChange = (selector1,selector2,pos) => {
+		pos == 'before' ? convert.$(selector1).insertBefore(selector2) : convert.$(selector1).insertAfter(selector2);
+	};
+    const loadTest = () => {
+    	classAllocation('#MainContent > .shopify-section','section');
+    	orderChange('.section-4','.section-2','after');
+    	orderChange('.section-3','.section-14','after');
+    	orderChange('.section-10','.section-4','after');
+    	orderChange('.section-11','.section-3','before');
+    	orderChange('.section-14','.section-13','before');
+    };
+    if (!convert.$('body').hasClass('vs-nf-test')) {
+        convert.$('body').addClass('vs-nf-test');
+        loadTest();
+    }
+});
