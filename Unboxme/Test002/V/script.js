cd7f640ca96445a1d3f315df53964d53fc253c4a
Harshjariwala <harshjariwala34.hj@gmail.com>
2023-07-06 10:25:44 +0530

    Unboxme test added

---
+++ Unboxme/Test002/V/script.js
@@ -0,0 +1,73 @@
+var convertInterval = setInterval(() => {
+    if (typeof convert != 'undefined' && convert.$(".pdp-galleries > .swiper-container.swiper-initialized").length > 0) {
+        clearInterval(convertInterval);
+        convert.$(document).ready(() => {
+            const loadTest = () => {
+                var oldSliderData = [];
+                var slides = '';
+                convert.$(".pdp-galleries > .swiper-container.swiper-initialized .swiper-slide").each(function() {
+                    var slideURL = convert.$(this).find('img').attr('src');
+                    var slideAlt = convert.$(this).find('img').attr('alt');
+                    if (slideURL != undefined) {
+                        oldSliderData.push({
+                            'imgURL': slideURL,
+                            'alt': slideAlt
+                        });
+                    }
+                });
+                slides = oldSliderData.map(function(data) {
+                    return `<div class="swiper-slide"><img src="${data.imgURL}" alt="${data.alt}" /></div>`;
+                });
+                convert.$(`<div class="main-slider"><div class="swiper customSwiper" id="custom-gallery"><div class="swiper-wrapper">${slides.join('')}</div><div class="swiper-pagination"></div></div><div class="swiper-button-prev"></div><div class="swiper-button-next"></div></div><div class="swiper customSwiperTumb" id="custom-tumbs"><div class="swiper-wrapper">${slides.join('')}</div></div><div class="count-wrap"><span class="current"></span> — <span class="total"></span><span class="extra-text"></span></div>`).insertAfter('.single-product__col:first-child');
+                var swiper1 = new Swiper(".customSwiperTumb", {
+                    loop: true,
+                    spaceBetween: 10,
+                    slidesPerView: 5,
+                    freeMode: true,
+                    watchSlidesProgress: true
+                });
+                var swiper2 = new Swiper(".customSwiper", {
+                    loop: true,
+                    spaceBetween: 10,
+                    effect: "fade",
+                    navigation: {
+                        nextEl: ".swiper-button-next",
+                        prevEl: ".swiper-button-prev",
+                    },
+                    pagination: {
+                        el: ".swiper-pagination",
+                        clickable: true,
+                    },
+                    thumbs: {
+                        swiper: swiper1,
+                    },
+                }).on('slideChange', function(e) {
+                    convert.$('.count-wrap .current').text(swiper2.activeIndex);
+                });
+                var totalCount = convert.$('.unboxme-test-v1 #custom-gallery .swiper-pagination .swiper-pagination-bullet').length;
+                var currentCount = 1;
+                var extraText = convert.$('#image-desc').text();
+                convert.$('.count-wrap .current').text(currentCount);
+                convert.$('.count-wrap .total').text(totalCount);
+                convert.$('.count-wrap .extra-text').text(extraText);
+                convert.$('body').on('click', '.swiper-button-prev', function() {
+                    currentCount == 1 ? currentCount = totalCount : currentCount--;
+                    convert.$('.count-wrap .current').text(currentCount);
+                    convert.$('#custom-gallery .swiper-pagination .swiper-pagination-bullet:nth-child(' + currentCount + ')').click();
+                });
+                convert.$('body').on('click', '.swiper-button-next', function() {
+                    currentCount < totalCount ? currentCount++ : currentCount = 1;
+                    convert.$('.count-wrap .current').text(currentCount);
+                    convert.$('#custom-gallery .swiper-pagination .swiper-pagination-bullet:nth-child(' + currentCount + ')').click();
+                });
+            }
+            let isMob = window.matchMedia("(max-width: 768px)").matches;
+            if (isMob) {
+                if (!convert.$('body').hasClass('unboxme-test-v1')) {
+                    convert.$('body').addClass('unboxme-test-v1');
+                    loadTest();
+                }
+            }
+        });
+    }
+});
