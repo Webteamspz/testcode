0d2c67f480aee675fc33f6f681f637b27cb7281d
Harshjariwala <harshjariwala34.hj@gmail.com>
2023-07-13 20:00:56 +0530

    Daily fixes

--- Fittrack/CTA Button Redirection Homepage/V/script.js
+++ Fittrack/CTA Button Redirection Homepage/V/script.js
@@ -4,9 +4,11 @@ convert.$(document).ready(function () {
         loadTest();
     }
     function loadTest() {
-    	convert.$('.MainnwMainWidgetproopeyrhOfferTw modal-opener .product-form__submit').off('click').on('click',function(e){
+    	convert.$('.MainnwMainWidgetproopeyrhOfferTw modal-opener .product-form__submit, .NWInnerdataExeutOfferTw.onlyForMobile modal-opener .product-form__submit').off('click').on('click',function(e){
     		e.preventDefault();
+    		convert.$('.product-popup-modal.atc-upsell-modal').css('display','none');
     		window.open('/pages/fittrack-pdp-shopify', '_self');
     	});
+    	convert.$('#link_shop-now a[href="/products/bmi-smart-scale-fittrack-dara"], nav .menu-drawer__menu a[href="/products/bmi-smart-scale-fittrack-dara"]:eq(1)').attr('href','/pages/fittrack-pdp-shopify');
     }
 });
